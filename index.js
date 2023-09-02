bd5176ecca98df738c4776a13847ec8c86c73961
divyavaland1609 <divyavaland479@gmail.com>
2023-09-02 22:48:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+function first()
+{
+    document.getElementById("ok").style.backgroundColor="red";
+    document.getElementById("p").style.backgroundColor="blue";
+    document.getElementById("o").style.backgroundColor="white";
+
+
+
+}
+function second()
+{
+    document.getElementById("ok").style.backgroundColor="orange";
+    document.getElementById("p").style.backgroundColor="black";
+    document.getElementById("o").style.backgroundColor="purple";
+
+
+}
+
+function third()
+{
+    document.getElementById("ok").style.backgroundColor="yellow";
+    document.getElementById("p").style.backgroundColor="purple";
+    document.getElementById("o").style.backgroundColor="blue";
+
+
+}
+
+function four()
+{
+    document.getElementById("ok").style.backgroundColor="green";
+    document.getElementById("p").style.backgroundColor="red";
+    document.getElementById("o").style.backgroundColor="yellow";
+
+
+}
+
+
+function five()
+{
+    document.getElementById("ok").style.backgroundColor="blue";
+    document.getElementById("p").style.backgroundColor="pink";
+    document.getElementById("o").style.backgroundColor="black";
+
+
+}
+function six()
+{
+    document.getElementById("ok").style.backgroundColor="indigo";
+    document.getElementById("p").style.backgroundColor="white";
+    document.getElementById("o").style.backgroundColor="pink";
+
+
+}
+
+function seven()
+{
+    document.getElementById("ok").style.backgroundColor="violet";
+    document.getElementById("p").style.backgroundColor="green";
+    document.getElementById("o").style.backgroundColor="red";
+
+
+}
